f6a546935f84ca22a4529cc29b3c6d74aa18e3c8
Brian Dobler <brianahueldobler@gmail.com>
2024-06-23 10:23:09 -0300

    Se vuelve cambio atrasç

---
+++ src/components/MensajeSaludo.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const tiempoActual = new Date();
+  const horaActual = tiempoActual.getHours();
+
+  let saludo = "";
+
+  if (horaActual < 12) {
+    saludo = "Buenos Días! 🌞";
+  } else if (horaActual < 18) {
+    saludo = "Buenas Tardes! 🌇";
+  } else {
+    saludo = "Buenas Noches! 🌙";
+  }
+</script>
+
+<h1 class="text-white text-4xl font-bold">{saludo}</h1>

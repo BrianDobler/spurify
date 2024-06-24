d098446a6a6f82dcc5b3def35eec87aece928964
Brian Dobler <brianahueldobler@gmail.com>
2024-06-23 21:49:41 -0300

    Se agrega transicion y escala del icono

--- src/components/Navegacion.jsx
+++ src/components/Navegacion.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 
 export const IconoNavAtras = () => (
-    <svg
-      height="16"
-      fill="currentColor"
-      width="16"
-      data-encore-id="icon"
-      role="img"
-      aria-hidden="true"
-      viewBox="0 0 16 16"
-    >
-      <path d="M11.03.47a.75.75 0 0 1 0 1.06L4.56 8l6.47 6.47a.75.75 0 1 1-1.06
-         1.06L2.44 8 9.97.47a.75.75 0 0 1 1.06 0z"></path>
-    </svg>
-  );
+  <svg
+    height="16"
+    fill="currentColor"
+    width="16"
+    data-encore-id="icon"
+    role="img"
+    aria-hidden="true"
+    viewBox="0 0 16 16"
+  >
+    <path
+      d="M11.03.47a.75.75 0 0 1 0 1.06L4.56 8l6.47 6.47a.75.75 0 1 1-1.06
+         1.06L2.44 8 9.97.47a.75.75 0 0 1 1.06 0z"
+    ></path>
+  </svg>
+);
 export function NavegacionAtras() {
   const NavAtras = () => {
     window.history.back();
@@ -22,9 +24,10 @@ export function NavegacionAtras() {
   return (
     <button
       onClick={NavAtras}
-      className="rounded-full p-2 bg-black bg-opacity-70"
+      className="rounded-full p-2 bg-black bg-opacity-70 transition
+      transform duration-200 ease-in-out hover:scale-105"
     >
-        <IconoNavAtras />
+      <IconoNavAtras />
     </button>
   );
 }
